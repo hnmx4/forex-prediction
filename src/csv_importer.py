41720f3c567a31ee2e9d0e044d4a25d56659b9c9
hnmx4 <hnmnty@gmail.com>
2018-06-23 03:31:36 +0900

    show candlestick chart using csv

--- src/csv_importer.py
+++ src/csv_importer.py
@@ -1,16 +1,47 @@
 import csv
 import os
+import matplotlib.pyplot as plt
+import mpl_finance as mpf
 
 
 class CsvImporter:
     def __init__(self):
-        historical_data_filename = os.path.join(
+        data_dir = os.path.join(
             os.path.abspath(os.path.dirname(os.path.dirname(__file__))),
-            "data",
-            "USDJPY_2015_all.csv"
-        )
-        with open(historical_data_filename, newline='') as f:
-            self.historical_data = list(csv.reader(f))
+            "data")
+
+        files = []
+        for _, _, f in os.walk(data_dir):
+            files = f
+
+        filenames = list(map(
+            lambda x: os.path.join(data_dir, x),
+            list(filter(lambda x: x.find(".csv") > 0, files))))
+
+        self.historical_data = []
+        for filename in filenames:
+            with open(filename, newline='') as f:
+                # historical data format:
+                # [date, time, opening, closing, low, high, volume]
+                self.historical_data.extend(list(csv.reader(f)))
+
+    def gen_prices(self, t):
+        price_type = {
+            'opening': 2,
+            'closing': 3,
+            'low': 4,
+            'high': 5
+        }
+
+        return list(map(lambda x: x[price_type[t]], self.historical_data))
 
     def show_graph(self):
-        print(self.historical_data)
+        ax = plt.subplot()
+        mpf.candlestick2_ohlc(
+            ax,
+            self.gen_prices('opening'),
+            self.gen_prices('high'),
+            self.gen_prices('low'),
+            self.gen_prices('closing')
+        )
+        plt.show()
