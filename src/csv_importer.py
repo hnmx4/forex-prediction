3dc175e2bc75b14bbd79a61012768110c8733d0f
hnmx4 <hnmnty@gmail.com>
2018-06-25 00:42:10 +0900

    add converter of a time period of candle chart

--- src/csv_importer.py
+++ src/csv_importer.py
@@ -2,6 +2,7 @@ import csv
 import os
 import matplotlib.pyplot as plt
 import mpl_finance as mpf
+import numpy as np
 
 
 class CsvImporter:
@@ -18,30 +19,56 @@ class CsvImporter:
             lambda x: os.path.join(data_dir, x),
             list(filter(lambda x: x.find(".csv") > 0, files))))
 
-        self.historical_data = []
+        hdata = []
         for filename in filenames:
             with open(filename, newline='') as f:
                 # historical data format:
                 # [date, time, opening, closing, low, high, volume]
-                self.historical_data.extend(list(csv.reader(f)))
+                hdata.extend(list(csv.reader(f)))
+        for row in hdata:
+            for i in range(2, 7):
+                row[i] = float(row[i])
+        self.historical_data = hdata
 
-    def gen_prices(self, t):
+    @staticmethod
+    def gen_prices(t, hdata):
         price_type = {
             'opening': 2,
             'closing': 3,
             'low': 4,
-            'high': 5
+            'high': 5,
+            'volume': 6
         }
 
-        return list(map(lambda x: x[price_type[t]], self.historical_data))
+        return list(map(lambda x: x[price_type[t]], hdata))
+
+    def transform_minutes(self, minutes, hdata):
+        ret = []
+        for i in range(0, len(hdata), minutes):
+            part = hdata[i:i + minutes]
+            ret.append([
+                part[0][0],  # data
+                part[0][1],  # time
+                part[0][2],  # opening
+                part[-1][3],  # closing
+                min(self.gen_prices("low", part)),  # low
+                max(self.gen_prices("high", part)),  # high
+                sum(self.gen_prices("volume", part))  # volume
+            ])
+
+        return ret
 
     def show_graph(self):
+        hdata_15 = self.transform_minutes(15, self.historical_data)
+
         ax = plt.subplot()
         mpf.candlestick2_ohlc(
             ax,
-            self.gen_prices('opening'),
-            self.gen_prices('high'),
-            self.gen_prices('low'),
-            self.gen_prices('closing')
+            self.gen_prices('opening', hdata_15),
+            self.gen_prices('high', hdata_15),
+            self.gen_prices('low', hdata_15),
+            self.gen_prices('closing', hdata_15),
+            0.5, 'r', 'b'
         )
+
         plt.show()
